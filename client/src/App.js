a9e3594a3b62a69f81e2c80bcd312f2298a7f512
IliyanManolov <killerood1@gmail.com>
2023-06-23 16:12:24 +0300

    fix imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 import './App.css';
 import CategoryList from './pages/CategoryList';
 import Dish from './pages/Dish';
